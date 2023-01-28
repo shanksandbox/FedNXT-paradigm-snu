f239d29e80949a7f55a1f86606118f4f535d7690
Shubhi Singh <shubhis921@gmail.com>
2023-01-28 13:29:53 +0530

    added web app with admin panel

---
+++ web app/resources/views/backend/customer/create.blade.php
@@ -0,0 +1,182 @@
+@extends('backend.layout.main') @section('content')
+@if(session()->has('not_permitted'))
+  <div class="alert alert-danger alert-dismissible text-center"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>{{ session()->get('not_permitted') }}</div>
+@endif
+<section class="forms">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header d-flex align-items-center">
+                        <h4>{{trans('file.Add Customer')}}</h4>
+                    </div>
+                    <div class="card-body">
+                        <p class="italic"><small>{{trans('file.The field labels marked with * are required input fields')}}.</small></p>
+                        {!! Form::open(['route' => 'customer.store', 'method' => 'post', 'files' => true]) !!}
+                        <div class="row">
+                            <div class="col-md-4 mt-4">
+                                <div class="form-group">
+                                    <input type="checkbox" name="both" value="1" />&nbsp;
+                                    <label>{{trans('file.Both Customer and Supplier')}}</label>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>{{trans('file.Customer Group')}} *</strong> </label>
+                                    <select required class="form-control selectpicker" id="customer-group-id" name="customer_group_id">
+                                        @foreach($lims_customer_group_all as $customer_group)
+                                            <option value="{{$customer_group->id}}">{{$customer_group->name}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>{{trans('file.name')}} *</strong> </label>
+                                    <input type="text" id="name" name="customer_name" required class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>{{trans('file.Company Name')}} <span class="asterisk">*</span></label>
+                                    <input type="text" name="company_name" class="form-control">
+                                    @if($errors->has('company_name'))
+                                   <span>
+                                       <strong>{{ $errors->first('company_name') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>{{trans('file.Email')}} <span class="asterisk">*</span></label>
+                                    <input type="email" name="email" placeholder="example@example.com" class="form-control">
+                                    @if($errors->has('email'))
+                                   <span>
+                                       <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>{{trans('file.Phone Number')}} *</label>
+                                    <input type="text" name="phone_number" required class="form-control">
+                                    @if($errors->has('phone_number'))
+                                   <span>
+                                       <strong>{{ $errors->first('phone_number') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>{{trans('file.Tax Number')}}</label>
+                                    <input type="text" name="tax_no" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>{{trans('file.Address')}} *</label>
+                                    <input type="text" name="address" required class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>{{trans('file.City')}} *</label>
+                                    <input type="text" name="city" required class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>{{trans('file.State')}}</label>
+                                    <input type="text" name="state" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>{{trans('file.Postal Code')}}</label>
+                                    <input type="text" name="postal_code" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>{{trans('file.Country')}}</label>
+                                    <input type="text" name="country" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-4 mt-4">
+                                <div class="form-group">
+                                    <input type="checkbox" name="user" value="1" />&nbsp;
+                                    <label>{{trans('file.Add User')}}</label>
+                                </div>
+                            </div>
+                            <div class="col-md-4 user-input">
+                                <div class="form-group">
+                                    <label>{{trans('file.UserName')}} *</label>
+                                    <input type="text" name="name" class="form-control">
+                                    @if($errors->has('name'))
+                                   <span>
+                                       <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="col-md-4 user-input">
+                                <div class="form-group">
+                                    <label>{{trans('file.Password')}} *</label>
+                                    <input type="password" name="password" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <input type="hidden" name="pos" value="0">
+                            <input type="submit" value="{{trans('file.submit')}}" class="btn btn-primary">
+                        </div>
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+@endsection
+
+@push('scripts')
+<script type="text/javascript">
+    $("ul#people").siblings('a').attr('aria-expanded','true');
+    $("ul#people").addClass("show");
+    $("ul#people #customer-create-menu").addClass("active");
+
+    $('.asterisk').hide();
+    $(".user-input").hide();
+
+    $('input[name="both"]').on('change', function() {
+        if ($(this).is(':checked')) {
+            $('.asterisk').show();
+            $('input[name="company_name"]').prop('required',true);
+            $('input[name="email"]').prop('required',true);
+        }
+        else{
+            $('.asterisk').hide();
+            $('input[name="company_name"]').prop('required',false);
+            $('input[name="email"]').prop('required',false);
+        }
+    });
+
+    $('input[name="user"]').on('change', function() {
+        if ($(this).is(':checked')) {
+            $('.user-input').show(300);
+            $('input[name="name"]').prop('required',true);
+            $('input[name="password"]').prop('required',true);
+        }
+        else{
+            $('.user-input').hide(300);
+            $('input[name="name"]').prop('required',false);
+            $('input[name="password"]').prop('required',false);
+        }
+    });
+</script>
+@endpush
